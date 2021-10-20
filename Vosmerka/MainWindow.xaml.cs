23d77820bbae0101e2a4c45ad4e3e4ebe0881644
Ваше имя <nothere@inthe.net>
2021-10-20 08:44:13 +0300

    A

--- Vosmerka/MainWindow.xaml.cs
+++ Vosmerka/MainWindow.xaml.cs
@@ -19,169 +19,211 @@ namespace Vosmerka
 {
     public partial class Product
     {
-
-        public Uri ImagePreviewService
+        // ссылка на картинку
+        // по ТЗ, если картинка не найдена, то должна выводиться картинка по-умолчанию
+        // в XAML-е можно это сделать средствами разметки, но там есть условие что вместо ссылки на картинку получен NULL
+        // у нас же возможна ситуация, когда в базе есть путь к картинке, но самой картинки в каталоге нет
+        // поэтому я сделал проверку наличия файла картинки и возвращаю картинку по-умолчанию, если нужной нет 
+        public Uri ImagePreview
         {
             get
             {
                 var imageName = System.IO.Path.Combine(Environment.CurrentDirectory, Image ?? "");
-                return System.IO.File.Exists(imageName) ? new Uri(imageName) : new Uri("pack://application:,,,/products/picture.jpg");
+                return System.IO.File.Exists(imageName) ? new Uri(imageName) : new Uri("pack://application:,,,/products/picture.png");
             }
         }
 
-        public string MinCostDiscount
+        public float DiscountFloat
         {
             get
             {
-                return (MinCostForAgent.ToString("#.##"));
+                return Convert.ToSingle(MinCostForAgent  );
+            }
+
+        }
+        public Boolean MinPrice
+        {
+            get
+            {
+                return MinCostForAgent < 100;
             }
         }
-
-
-
+        public string ProductString
+        {
+            get
+            {
+                return Title ?? "";
+            }
+        }
     }
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        private IEnumerable<Product> _ProductLIst;
 
-        public IEnumerable<Product> ProductList
+        private List<Product> _ServiceList;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public List<Product> ServiceList
         {
             get
             {
-                var res = _ProductLIst;
+                var FilteredServiceList = _ServiceList.FindAll(item =>
+                item.DiscountFloat >= CurrentDiscountFilter.Item1 &&
+              item.DiscountFloat < CurrentDiscountFilter.Item2);
 
-                // у объекта "Все улицы" Id=0, т.к. он взят не из базы, а создан в приложении
-                // если выбрана улица, то выбираем только объекты с такой улицей
-                if (_StreetFilterValue > 0)
-                    res = res.Where(ai => ai.ProductTypeID == _StreetFilterValue);
 
-                if (SortAsc) res = res.OrderBy(ai => ai.MinCostDiscount);
-                else res = res.OrderByDescending(ai => ai.MinCostDiscount);
+
                 if (SearchFilter != "")
-                    res = res.Where(ai => ai.ProductType.Title.IndexOf(SearchFilter, StringComparison.OrdinalIgnoreCase) >= 0);
+                    FilteredServiceList = FilteredServiceList.Where(item =>
+                        item.Title.IndexOf(SearchFilter, StringComparison.OrdinalIgnoreCase) != -1 ||
+                        item.ProductString.IndexOf(SearchFilter, StringComparison.OrdinalIgnoreCase) != -1).ToList();
 
-
-                return res;
-
-
+                if (SortPriceAscending)
+                    return FilteredServiceList.OrderBy(item => (item.MinCostForAgent)).ToList();
+                else
+                    return FilteredServiceList.OrderByDescending(item => (item.MinCostForAgent)).ToList();
             }
             set
             {
-                _ProductLIst = value;
+                _ServiceList = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("ServiceList"));
+                    PropertyChanged(this, new PropertyChangedEventArgs("ServicesCount"));
+                    PropertyChanged(this, new PropertyChangedEventArgs("FilteredServicesCount"));
+                }
+
             }
         }
-
-        public List<ProductType> StreetsList { get; set; }
 
         public MainWindow()
         {
             InitializeComponent();
             this.DataContext = this;
-            ProductList = Class1.DB.Product.ToList();
-            StreetsList = Class1.DB.ProductType.ToList();
-            StreetsList.Insert(0, new ProductType { Title = "Все типы материала" });
+            ServiceList = Class1.DB.Product.ToList();
         }
 
-        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        private void ExitButtonClick(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
         }
-
-        private bool _SortAsc = true;
-        public bool SortAsc
+        private void OrdProvidClick(object sender, RoutedEventArgs e)
         {
-            get
-            {
-                return _SortAsc;
-            }
-            set
-            {
-                _SortAsc = value;
-                if (PropertyChanged != null)
-                {
-                    PropertyChanged(this, new PropertyChangedEventArgs("ProductList"));
-                }
-            }
+            
         }
 
+        private void SearchFilterTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private Boolean _SortPriceAscending = true;
+        public Boolean SortPriceAscending
+        {
+            get { return _SortPriceAscending; }
+            set
+            {
+                _SortPriceAscending = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("ServiceList"));
+                }
+
+            }
+
+        }
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
-            SortAsc = (sender as RadioButton).Tag.ToString() == "1";
+            SortPriceAscending = (sender as RadioButton).Tag.ToString() == "1";
         }
 
 
-        private int _StreetFilterValue = 0;
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        private Tuple<float, float> _CurrentDiscountFilter = Tuple.Create(float.MinValue, float.MaxValue);
 
-        public int StreetFilterValue
+        public Tuple<float, float> CurrentDiscountFilter
         {
             get
             {
-                return _StreetFilterValue;
+                return _CurrentDiscountFilter;
             }
             set
             {
-                _StreetFilterValue = value;
+                _CurrentDiscountFilter = value;
                 if (PropertyChanged != null)
                 {
-                    PropertyChanged(this, new PropertyChangedEventArgs("ProductList"));
+                    // при изменении фильтра список перерисовывается
+                    PropertyChanged(this, new PropertyChangedEventArgs("ServiceList"));
                 }
             }
         }
 
-        private void StreetFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        public List<string> FilterByDiscountNamesList
         {
-            StreetFilterValue = (StreetFilter.SelectedItem as ProductType).ID;
+            get
+            {
+                return FilterByDiscountValuesList
+                    .Select(item => item.Item1)
+                    .ToList();
+            }
         }
+
+        private List<Tuple<string, float, float>> FilterByDiscountValuesList =
+          new List<Tuple<string, float, float>>() {
+        Tuple.Create("Все записи", 0f, 10000f),
+        Tuple.Create("от 100 до 500", 0f, 500f),
+        Tuple.Create("от 500 до 2000", 500f,2000f),
+        Tuple.Create("от 2000 до 5000", 2000f, 10000f),
+        Tuple.Create("от 5000 до 30000", 10000f, 50000f)
+    }; private void DiscountFilterComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            CurrentDiscountFilter = Tuple.Create(
+                FilterByDiscountValuesList[DiscountFilterComboBox.SelectedIndex].Item2,
+                FilterByDiscountValuesList[DiscountFilterComboBox.SelectedIndex].Item3
+            );
+        }
+
         private string _SearchFilter = "";
         public string SearchFilter
         {
-            get
-            {
-                return _SearchFilter;
-            }
+            get { return _SearchFilter; }
             set
             {
                 _SearchFilter = value;
                 if (PropertyChanged != null)
                 {
-                    PropertyChanged(this, new PropertyChangedEventArgs("ProductList"));
+                    // при изменении фильтра список перерисовывается
+                    PropertyChanged(this, new PropertyChangedEventArgs("ServiceList"));
+                    PropertyChanged(this, new PropertyChangedEventArgs("ServicesCount"));
+                    PropertyChanged(this, new PropertyChangedEventArgs("FilteredServicesCount"));
+
                 }
             }
         }
-        public int SelectedRows
-        {
-            get
-            {
-                return ProductList.Count();
-            }
-        }
-
-        public int TotalRows
-        {
-            get
-            {
-                return _ProductLIst.Count();
-            }
-        }
-        private void Invalidate()
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs("ProductList"));
-                PropertyChanged(this, new PropertyChangedEventArgs("SelectedRows"));
-                PropertyChanged(this, new PropertyChangedEventArgs("TotalRows"));
-            }
-        }
-
-        private void SearchFilter_KeyUp(object sender, KeyEventArgs e)
+        private void TextBox_KeyUp(object sender, KeyEventArgs e)
         {
             SearchFilter = SearchFilterTextBox.Text;
         }
 
+        public int ProductCount
+        {
+            get
+            {
+                return _ServiceList.Count;
+            }
+
+        }
+        public int FilteredProductCount
+        {
+            get
+            {
+                return ServiceList.Count;
+            }
+        }
+
+        private void ProductListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
